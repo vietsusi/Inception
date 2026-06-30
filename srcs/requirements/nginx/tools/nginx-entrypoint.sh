0626c23a0b0921abab8711086b8053d2ff9e3767
Viet Nguyen <susinguyen.2025@gmail.com>
2026-06-30 16:42:54 +0300

    modify nginx-entrypoint.sh to add location for php

--- srcs/requirements/nginx/tools/nginx-entrypoint.sh
+++ srcs/requirements/nginx/tools/nginx-entrypoint.sh
@@ -17,13 +17,22 @@ server {
     ssl_certificate /etc/nginx/ssl/cert.crt;
     ssl_certificate_key /etc/nginx/ssl/cert.key;
     ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256;
-
+    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384;
     root /var/www/html;
     index index.php index.html index.htm;
 
     location / {
         try_files \$uri \$uri/ /index.php?\$args;
+    }
+
+    location ~ [^/]\.php(/|\$) {
+        try_files \$fastcgi_script_name =404;
+        fastcgi_pass wordpress:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param PATH_INFO \$fastcgi_path_info;
+        fastcgi_split_path_info ^(.+\.php)(/.*)\$;
+        include fastcgi_params;
     }
 }
 EOF
